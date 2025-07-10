4eac8729526ddf56cda85fd9d0678ecb70e8d599
Avinash KUMAR <Avinashmadhukar4@gmail.com>
2025-07-10 23:54:59 +0000

    on background page of website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen bg-transparent">
       <Header />
       <main className="flex-1">
         <Hero />
