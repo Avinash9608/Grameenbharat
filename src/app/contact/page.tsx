add5bc179143fab483c885bcf7fd71082fc23631
Avinash KUMAR <Avinashmadhukar4@gmail.com>
2025-07-12 06:41:53 +0000

    imge not displying

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,7 +27,7 @@ const ContactPage = () => {
     }
 
     return (
-        <div className="flex flex-col min-h-screen bg-background">
+        <div className="flex flex-col min-h-screen">
             <Header />
 
             <main className="flex-1">
@@ -52,7 +52,7 @@ const ContactPage = () => {
                     </div>
                 </section>
 
-                <section className="py-16 md:py-24">
+                <section className="py-16 md:py-24 bg-background">
                     <div className="container mx-auto px-4 md:px-6">
                         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                             <div className="lg:col-span-5">
