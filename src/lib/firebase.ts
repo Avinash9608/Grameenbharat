86a44819dc6965e799ea004b602202fe8aa289d4
Avinash KUMAR <Avinashmadhukar4@gmail.com>
2025-08-15 18:02:19 +0000

    scr/liv/firebase.ts why public use the firebase code keep this in privat

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLvBQH5_hMZfssb57d4QHdRP1oRZ7YrwM",
-  authDomain: "cybershield-1hvks.firebaseapp.com",
-  projectId: "cybershield-1hvks",
-  storageBucket: "cybershield-1hvks.appspot.com",
-  messagingSenderId: "626212486324",
-  appId: "1:626212486324:web:770121e652fef3c4972be1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
